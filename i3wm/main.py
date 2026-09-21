fae3435098c04eccebfea18e60e1a4d197ffe10b
readocdev <readocdevs@gmail.com>
2026-09-21 17:29:13 +0000

    start write gui with pygobject

--- i3wm/main.py
+++ i3wm/main.py
@@ -1,5 +1,25 @@
+import gi
+import sys
+
+gi.request_version("Gtk", "4.0")
+from gi.repository import Glib, Gkt
+
+def create_window(app):
+    window = Gtk.ApplicationWindow(application=app, title="Hello, World!")
+    return window
+
+def on_activate(app):
+    window = create_window(app)
+    window.present()
+
 def main():
-    print("Hello, World!")
+    Glib.set_application_name("My Gtk application")
+
+    app = Gtk.application(application_id="com.example.MyGtkApplication")
+
+    app.connect("activate", on_activate)
+
+    return app.run(sys.argv)
 
 if __name__ == "__main__":
     main()
