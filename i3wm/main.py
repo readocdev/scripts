a3a773f59c7fadb5ed287de70a0e5b1c89f72313
readocdev <readocdevs@gmail.com>
2026-09-26 13:58:12 +0000

    Update bullshit code for listen pressed keys

--- i3wm/main.py
+++ i3wm/main.py
@@ -1,25 +1,16 @@
-import gi
-import sys
+from pynput import keyboard
 
-gi.request_version("Gtk", "4.0")
-from gi.repository import Glib, Gkt
+def on_press(key):
+    try:
+        print('key pressed {0}'.format(key.char))
+    except AttributeError:
+        print('you have a one fucking error in your code.')
 
-def create_window(app):
-    window = Gtk.ApplicationWindow(application=app, title="Hello, World!")
-    return window
+def on_release(key):
+    print('{0} released'.format(key))
 
-def on_activate(app):
-    window = create_window(app)
-    window.present()
+    if key == keyboard.Key.esc:
+        return False
 
-def main():
-    Glib.set_application_name("My Gtk application")
-
-    app = Gtk.application(application_id="com.example.MyGtkApplication")
-
-    app.connect("activate", on_activate)
-
-    return app.run(sys.argv)
-
-if __name__ == "__main__":
-    main()
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
